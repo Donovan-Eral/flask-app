1e78eaeb8b8dec16306dc825a7fdd9d9997efef2
Donovan-Eral <donovaneral@gmail.com>
2022-04-22 17:42:40 -0700

    modified app.py

--- app.py
+++ app.py
@@ -1,7 +1,14 @@
 from flask import Flask
+from src.extensions import db
+from src.routes.user import user
+
+
 app = Flask(__name__)
+app.config.from_object('src.config.Config')
+db.init_app(app)
+app.register_blueprint(user)
 
 
-@app.route('/')
-def hello_world():
-    return 'Hello, Austin!'
+# @app.route('/')
+# def hello_world():
+#     return 'Hello, Austin!'
---
+++ src/config.py
@@ -0,0 +1,4 @@
+class Config:
+    # Database
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///user.db'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False

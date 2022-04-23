746a7b28a19ad586ec7b70e4422229feb11f6220
Donovan-Eral <donovaneral@gmail.com>
2022-04-22 18:33:19 -0700

    test

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
 from flask import Flask
-# from src.extensions import db
-# from src.routes.user import user
+from src.extensions import db
+from src.routes.user import user
 
 
 app = Flask(__name__)
-# app.config.from_object('src.config.Config')
-# db.init_app(app)
-# app.register_blueprint(user)
+app.config.from_object('src.config.Config')
+db.init_app(app)
+app.register_blueprint(user)
 
 
 @app.route('/')

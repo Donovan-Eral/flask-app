43c75d95e4dbb4132f64708978710149c6cf3ae5
Donovan-Eral <donovaneral@gmail.com>
2022-04-22 18:01:02 -0700

    test

--- app.py
+++ app.py
@@ -4,6 +4,11 @@ from src.routes.user import user
 
 
 app = Flask(__name__)
-app.config.from_object('src.config.Config')
-db.init_app(app)
-app.register_blueprint(user)
+# app.config.from_object('src.config.Config')
+# db.init_app(app)
+# app.register_blueprint(user)
+
+
+@app.route('/')
+def index():
+    return 'Hello, World!'
